4601de1ba2ee90efa8fd826c17d781ec0c1ff522
sohel-sk <sohelsheikh9623@gmail.com>
2025-12-29 18:38:05 +0530

    add : jest, prettier setup and module error resolve

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,7 @@
 import express = require("express");
+import cors = require("cors");
 
 const app = express();
+app.use(cors())
 
 export = app;
--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -4,11 +4,13 @@ dotenv.config();
 interface Config {
     PORT: number;
     nodeEnv: string;
+    DATABASE_URL: string;
 }
 
 const config: Config = {
     PORT: process.env.PORT ? parseInt(process.env.PORT) : 3000,
     nodeEnv: process.env.NODE_ENV || "development",
+    DATABASE_URL: process.env.DATABASE_URL || "",
 };
 
 export = config;
