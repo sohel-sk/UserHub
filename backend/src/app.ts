b41dbd46e0a2df086015bc543d849f0f5da7ede0
sohel-sk <sohelsheikh9623@gmail.com>
2025-12-30 00:17:54 +0530

    add : route protection middleware setup done

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,11 +3,13 @@ import cors from 'cors';
 import prisma from "./prisma/prismaClient";
 import authRoutes from "./routes/auth.routes";
 import cookieParser from "cookie-parser";
+import { authenticate } from "./middlewares/auth.middleware";
 const app = express();
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(cookieParser());
 app.use(cors());
 app.use("/auth", authRoutes)
+
 
 export = app;
---
+++ backend/src/middlewares/auth.middleware.ts
@@ -0,0 +1,67 @@
+import { Request,Response,NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import prisma from "../prisma/prismaClient";
+import config = require("../config/config");
+
+interface JwtPayload { 
+    userId: string;
+    role: string;
+}
+
+export interface AuthenticatedRequest extends Request { 
+    user?: {
+        id: string;
+        role: string;
+    }
+}
+
+export const authenticate = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => { 
+
+    try {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader || !authHeader.startsWith("Bearer ")) { 
+            return res.status(401).json({
+                message: "Authentication required"
+            });
+        }
+
+        const token = authHeader.split(" ")[1];
+
+        const decoded = jwt.verify(
+            token as string,
+            config.JWT_SECRET
+        ) as JwtPayload;
+
+
+        const user = await prisma.user.findUnique({
+            where: {
+                id:decoded.userId
+            },
+        })
+
+        if (!user) { 
+            return res.status(401).json({
+                message: "Invalid token"
+            })
+        }
+        
+        if (user.status !== "ACTIVE") { 
+            return res.status(403).json({
+                message: "Account is deactivated. Contact the Admin for the activation of the account",
+            });
+        }
+
+        req.user = {
+            id: user.id,
+            role:user.role,
+        }
+
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            message: "Invalid or expired token",
+        });
+        
+    }
+}
