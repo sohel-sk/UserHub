730b061f89dc8a88652a65da9d6c5b85782179b0
sohel-sk <sohelsheikh9623@gmail.com>
2025-12-30 22:24:12 +0530

    fix : deployment error

--- frontend/src/lib/passwordCheck.ts
+++
@@ -1,7 +0,0 @@
-export const passwordRequirements = [
-        { label: "At least 8 characters", met: password.length >= 8 },
-        { label: "Contains uppercase letter", met: /[A-Z]/.test(password) },
-        { label: "Contains lowercase letter", met: /[a-z]/.test(password) },
-        { label: "Contains number", met: /[0-9]/.test(password) },
-    ]
-
